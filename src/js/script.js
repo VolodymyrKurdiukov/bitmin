2060fe443e45e1726f3c251b5e25d0f258d7e79c
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-08-17 23:35:50 +0300

    header

--- src/js/script.js
+++ src/js/script.js
@@ -26,3 +26,19 @@ const isMobile = {
 			isMobile.Windows());
 	}
 };
+
+if (isMobile.any()) {
+	document.body.classList.add('touch');
+
+	let menuArrows = document.querySelectorAll('.menu__arrow');
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.parentElement.classList.toggle('active');
+			});
+		}
+	}
+} else {
+	document.body.classList.add('pc');
+}
