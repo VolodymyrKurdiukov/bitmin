78f3fee93c67d47c7575340b9ebe249672d19324
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-08-17 16:30:49 +0300

    header

--- src/js/script.js
+++ src/js/script.js
@@ -3,27 +3,26 @@
 const isMobile = {
 	Android: function () {
 		return navigator.userAgent.match(/Android/i);
-	};
-},
-	
-BlackBerry: function() {
-	return navigator.userAgent.match(/BlackBerry/i);
-},
-ios: function() {
-	return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-},
-Opera: function() {
-	return
-}
-navigator.userAgent.match(/Opera Mini/i);
 	},
-Windows: function() {
-	return navigator.userAgent.match(/IEMobile/i);
-},
-any: function() {
-	return (
-		isMobile.Android() ||
-		isMobile.BlackBerry() ||
-		isMobile.ios() ||
-		isMobile.Opera() ||
-		isMobile.Windows());
+
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	ios: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.ios() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
