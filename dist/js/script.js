f555abd04c2e8827db4edd0f975388451f413c3f
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-08-18 11:14:17 +0300

    headerFinal

--- dist/js/script.js
+++ dist/js/script.js
@@ -42,3 +42,33 @@ if (isMobile.any()) {
 } else {
 	document.body.classList.add('pc');
 }
+
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu) {
+	const menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle("lock");
+		iconMenu.classList.toggle("active");
+		menuBody.classList.toggle("active");
+	});
+}
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener("click", onMenuLinkClick);
+	});
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYoffset - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
